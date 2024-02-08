33f13ca9e6cc543e3cd1277817910929bd9366be
EJVillano <155346924+EJVillano@users.noreply.github.com>
2024-02-08 18:37:00 +0800

    Add files via upload

--- index.js
+++ index.js
@@ -6,9 +6,9 @@ document.querySelector(".img1").setAttribute("src", "./images/dice"+randomNumber
 document.querySelector(".img2").setAttribute("src", "./images/dice"+randomNumber2+".png");
 
 if(randomNumber1 > randomNumber2){
-    document.getElementById("result").textContent ="(^▽^)👍 Winner is player 1 ";
+    document.getElementById("result").textContent ="(^▽^)👍 Mag manila ka ";
 }else if (randomNumber1 < randomNumber2){
-    document.getElementById("result").textContent ="Winner is player 2 👍(^▽^)"; 
+    document.getElementById("result").textContent ="mag lucena ka 👍(^▽^)"; 
 }else{
     document.getElementById("result").textContent ="Refresh me"
 }
